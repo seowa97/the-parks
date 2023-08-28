0a1f2d6e34e77dd375b10802c58fb90f5ed84cae
Seong <96964068+seowa97@users.noreply.github.com>
2023-08-27 23:46:30 -0400

    some changes to run on pc, no additions

--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,6 @@ def index():
     return render_template('index.html', title='Home')
 
 
-# @app.route('/login')
-# def login():
-#     form = LoginForm()
-#     return render_template('login.html', title='Sign In', form=form)
-
 @app.route('/login')
 def login():
     form = LoginForm()
